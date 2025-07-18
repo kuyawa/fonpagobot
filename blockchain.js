940c81a6bf1ec63ef89e0a6616c030864c28d37d
Kuyawa <kuyawakata@gmail.com>
2025-07-18 17:36:33 -0400

    jetton transfer

--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,6 @@
 // BLOCKCHAIN
 
-const { TonClient, WalletContractV3R1, Address, internal, comment, fromNano, toNano } = require('@ton/ton')
+const { TonClient, WalletContractV3R1, WalletContractV4, Address, internal, comment, fromNano, toNano } = require('@ton/ton')
 const { keyPairFromSeed, mnemonicNew, mnemonicToPrivateKey } = require('@ton/crypto')
 const { beginCell, SendMode, storeMessageRelaxed } = require('@ton/core')
 const utils = require('./utils')
